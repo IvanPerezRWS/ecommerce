22e29d5d893ea80794d4c2d5de65df8a1843cd57
IvanPerezRWS <ivan.perez.rws@gmail.com>
2021-10-26 12:30:32 -0500

    Added ProductTag and Tag js models

--- models/Tag.js
+++ models/Tag.js
@@ -5,8 +5,17 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
     // define columns
+  { 
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: TransitionEvent,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
