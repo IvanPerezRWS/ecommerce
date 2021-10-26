5473a2d62a58b6ac5dec291ead9979a9e1c4c3b4
IvanPerezRWS <ivan.perez.rws@gmail.com>
2021-10-26 12:20:52 -0500

    Created product and category js models

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,5 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, STRING, INTEGER } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -7,10 +7,46 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
+// define table columns and configuration
 Product.init(
-  {
+{
+  id: {
     // define columns
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
   },
+  product_name: {
+    //product name
+    type: DataTypes.STRING,
+    allowNull: false
+
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true
+    }
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true
+    }
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'category',
+      key: 'id'
+    }
+  }
+},
+
   {
     sequelize,
     timestamps: false,
@@ -18,6 +54,7 @@ Product.init(
     underscored: true,
     modelName: 'product',
   }
+
 );
 
 module.exports = Product;
